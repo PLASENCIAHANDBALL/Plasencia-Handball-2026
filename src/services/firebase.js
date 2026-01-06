0d7bafd7dc81fcb4e13a349c9a66824bae4bf60c
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-07 00:00:24 +0100

    Añadir claves reales de Firebase

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'TU_API_KEY',
-  authDomain: 'TU_AUTH_DOMAIN',
-  projectId: 'TU_PROJECT_ID',
-  storageBucket: 'TU_STORAGE_BUCKET',
-  messagingSenderId: 'TU_MESSAGING_SENDER_ID',
-  appId: 'TU_APP_ID'
+  apiKey: "AIzaSyAfJmeY6xvXxMHK_-dXbb27604qd-Tcwa8",
+  authDomain: "plasencia-handball-2026.firebaseapp.com",
+  projectId: "plasencia-handball-2026",
+  storageBucket: "plasencia-handball-2026.firebasestorage.app",
+  messagingSenderId: "789132633460",
+  appId: "1:789132633460:web:a3147da41ed8760b34b821"
 };
 
 const app = initializeApp(firebaseConfig);

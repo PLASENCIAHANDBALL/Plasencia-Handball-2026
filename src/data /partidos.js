78b61264301ac7a23603a8acfc06be67d31f2b4c
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-06 23:20:41 +0100

    Añadir datos base de partidos

---
+++ src/data /partidos.js
@@ -0,0 +1,29 @@
+export const partidos = [
+  {
+    id: '1',
+    grupo: 'A',
+    local: 'BM Plasencia',
+    visitante: 'BM Cáceres',
+    golesLocal: 12,
+    golesVisitante: 10,
+    estado: 'En juego'
+  },
+  {
+    id: '2',
+    grupo: 'A',
+    local: 'BM Mérida',
+    visitante: 'BM Don Benito',
+    golesLocal: 0,
+    golesVisitante: 0,
+    estado: 'Pendiente'
+  },
+  {
+    id: '3',
+    grupo: 'B',
+    local: 'BM Badajoz',
+    visitante: 'BM Coria',
+    golesLocal: 22,
+    golesVisitante: 20,
+    estado: 'Finalizado'
+  }
+];

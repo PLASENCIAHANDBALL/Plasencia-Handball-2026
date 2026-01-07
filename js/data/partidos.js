6c5ec507b5e90df854fe3826a94c54b6948aa502
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-07 02:24:09 +0100

    Add initial partidos data and storage functions

---
+++ js/data/partidos.js
@@ -0,0 +1,25 @@
+const partidosIniciales = [
+  {
+    id: 1,
+    local: "Plasencia BM",
+    visitante: "Cáceres BM",
+    golesLocal: 0,
+    golesVisitante: 0
+  },
+  {
+    id: 2,
+    local: "Navalmoral",
+    visitante: "Badajoz",
+    golesLocal: 0,
+    golesVisitante: 0
+  }
+];
+
+function obtenerPartidos() {
+  const guardados = localStorage.getItem("partidos");
+  return guardados ? JSON.parse(guardados) : partidosIniciales;
+}
+
+function guardarPartidos(partidos) {
+  localStorage.setItem("partidos", JSON.stringify(partidos));
+}

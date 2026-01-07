9422f293686471d71d9de4f03a3c0e024653b79b
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-07 01:58:54 +0100

    Add initial partidos data to partidos.js

---
+++ src/data/partidos.js
@@ -0,0 +1,16 @@
+export const partidosIniciales = [
+  {
+    id: '1',
+    local: 'Plasencia BM',
+    visitante: 'Cáceres BM',
+    golesLocal: 0,
+    golesVisitante: 0
+  },
+  {
+    id: '2',
+    local: 'Navalmoral',
+    visitante: 'Badajoz',
+    golesLocal: 0,
+    golesVisitante: 0
+  }
+];

bb4b057fce9a88a201997e649589aa574f96f998
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-07 03:04:56 +0100

    Crear grupos.js

---
+++ js/data/grupos.js
@@ -0,0 +1,20 @@
+const grupos = [
+  {
+    nombre: "Grupo A",
+    equipos: [
+      "Plasencia",
+      "Cáceres",
+      "Navalmoral",
+      "Badajoz"
+    ]
+  },
+  {
+    nombre: "Grupo B",
+    equipos: [
+      "Don Benito",
+      "Mérida",
+      "Zafra",
+      "Almendralejo"
+    ]
+  }
+];

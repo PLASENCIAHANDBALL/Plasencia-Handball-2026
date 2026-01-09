f8c792032c71ceba0e0b966d3e350af0a0c708eb
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-09 17:47:31 +0100

    Actualizar clasificacion.js

--- js/data/clasificacion.js
+++ js/data/clasificacion.js
@@ -1,39 +1,3 @@
-function calcularClasificacion() {
-  const tabla = {};
-
-  partidos.forEach(p => {
-    if (!tabla[p.local]) tabla[p.local] = crearEquipo(p.local);
-    if (!tabla[p.visitante]) tabla[p.visitante] = crearEquipo(p.visitante);
-
-    tabla[p.local].pj++;
-    tabla[p.visitante].pj++;
-
-    tabla[p.local].gf += p.golesLocal;
-    tabla[p.local].gc += p.golesVisitante;
-    tabla[p.visitante].gf += p.golesVisitante;
-    tabla[p.visitante].gc += p.golesLocal;
-
-    if (p.golesLocal > p.golesVisitante) {
-      tabla[p.local].pg++;
-      tabla[p.local].puntos += 2;
-      tabla[p.visitante].pp++;
-    } else if (p.golesLocal < p.golesVisitante) {
-      tabla[p.visitante].pg++;
-      tabla[p.visitante].puntos += 2;
-      tabla[p.local].pp++;
-    } else {
-      tabla[p.local].pe++;
-      tabla[p.visitante].pe++;
-      tabla[p.local].puntos++;
-      tabla[p.visitante].puntos++;
-    }
-  });
-
-  return Object.values(tabla).sort(
-    (a, b) => b.puntos - a.puntos || (b.gf - b.gc) - (a.gf - a.gc)
-  );
-}
-
 function crearEquipo(nombre) {
   return {
     nombre,
@@ -50,14 +14,15 @@ function crearEquipo(nombre) {
 function calcularClasificacionFiltrada(categoria, genero, grupo) {
   const tabla = {};
 
+  // 👉 SOLO partidos finalizados y filtrados
   const partidosValidos = partidos.filter(p =>
-  p.estado === "finalizado" &&
-  p.categoria === categoria &&
-  p.genero === genero &&
-  (!grupo || p.grupo === grupo)
-);
+    p.estado === "finalizado" &&
+    p.categoria === categoria &&
+    p.genero === genero &&
+    (!grupo || p.grupo === grupo)
+  );
 
-  partidosFiltrados.forEach(p => {
+  partidosValidos.forEach(p => {
     if (!tabla[p.local]) tabla[p.local] = crearEquipo(p.local);
     if (!tabla[p.visitante]) tabla[p.visitante] = crearEquipo(p.visitante);
 
@@ -73,11 +38,13 @@ function calcularClasificacionFiltrada(categoria, genero, grupo) {
       tabla[p.local].pg++;
       tabla[p.local].puntos += 2;
       tabla[p.visitante].pp++;
-    } else if (p.golesLocal < p.golesVisitante) {
+    } 
+    else if (p.golesLocal < p.golesVisitante) {
       tabla[p.visitante].pg++;
       tabla[p.visitante].puntos += 2;
       tabla[p.local].pp++;
-    } else {
+    } 
+    else {
       tabla[p.local].pe++;
       tabla[p.visitante].pe++;
       tabla[p.local].puntos++;
@@ -85,11 +52,10 @@ function calcularClasificacionFiltrada(categoria, genero, grupo) {
     }
   });
 
-  return Object.values(tabla).sort((a, b) => {
-    if (b.puntos !== a.puntos) return b.puntos - a.puntos;
-    const difA = a.gf - a.gc;
-    const difB = b.gf - b.gc;
-    if (difB !== difA) return difB - difA;
-    return b.gf - a.gf;
-  });
+  return Object.values(tabla).sort(
+    (a, b) =>
+      b.puntos - a.puntos ||
+      (b.gf - b.gc) - (a.gf - a.gc) ||
+      b.gf - a.gf
+  );
 }

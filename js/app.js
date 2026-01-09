5afda98fe6de66daf631360c61f8919f759d943e
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-09 11:50:25 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -860,7 +860,7 @@ mostrarHome();
 window.addEventListener("load", () => {
   const splash = document.getElementById("splash");
   if (splash) {
-    setTimeout(() => splash.style.display = "none", 1200);
+    setTimeout(() => splash.style.display = "none", 1100);
   }
   if (adminActivo) {
   document.getElementById("admin-fab")?.classList.add("admin-activo");

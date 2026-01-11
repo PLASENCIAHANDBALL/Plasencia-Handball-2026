0e15fc2e761510417cbeded390b34a9e2aeebaab
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-11 01:57:31 +0100

    Actualizar partidos.js

--- js/data/partidos.js
+++ js/data/partidos.js
@@ -1,26 +1,3 @@
-const partidosIniciales = [
-  {
-    id: 1,
-    local: "Plasencia BM",
-    visitante: "Cáceres BM",
-    golesLocal: 0,
-    golesVisitante: 0,
-    lugar: "Pabellón Municipal",
-    hora: "18:00",
-    categoria: "Infantil",
-    genero: "Masculino",
-    grupo: "Grupo A"
-  }, // ✅ COMA AQUÍ
-
-  {
-    id: 2,
-    local: "Navalmoral",
-    visitante: "Badajoz",
-    golesLocal: 0,
-    golesVisitante: 0
-  }
-];
-
 function obtenerPartidos() {
   const guardados = localStorage.getItem("partidos");
   return guardados ? JSON.parse(guardados) : partidosIniciales;

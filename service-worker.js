df409711558ad5aa52f42b70e917a0f58abb932f
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-12 21:59:46 +0100

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,21 @@
-const CACHE_NAME = "plasencia-handball-v1";
+const CACHE_NAME = "plasencia-handball-v2";
 
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/css/style.css",
-  "/js/app.js",
-  "/js/data/partidos.js",
-  "/js/data/grupos.js",
-  "/js/data/clasificacion.js",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./css/style.css",
+  "./js/app.js",
+  "./js/data/partidos.js",
+  "./js/data/grupos.js",
+  "./js/data/clasificacion.js",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(FILES_TO_CACHE))
+      .catch((err) => console.error("Cache error:", err))
   );
 });
 

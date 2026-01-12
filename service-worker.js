70e6e708ddbcaea5d652fd9a0e88188a0fa2a5bc
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-12 22:46:21 +0100

    Actualizar service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "plasencia-handball-v2";
+const CACHE_NAME = "plasencia-handball-v3";
 
 const FILES_TO_CACHE = [
   "./",
@@ -31,6 +31,20 @@ self.addEventListener("fetch", (e) => {
   e.respondWith(
     caches.match(e.request).then(
       (response) => response || fetch(e.request)
+    )
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
     )
   );
 });

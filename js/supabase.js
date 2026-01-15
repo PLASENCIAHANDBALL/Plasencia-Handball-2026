6d07caa3792210b89cdb3fe93187c858757999a1
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-01-15 22:50:51 +0100

    Actualizar supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,11 +1,7 @@
 // supabase.js
 import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
-// ⚠️ PON AQUÍ TUS DATOS
-const SUPABASE_URL = "https://manobzroedsaxagugcvn.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbm9ienJvZWRzYXhhZ3VnY3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNzA5MTMsImV4cCI6MjA4MzY0NjkxM30.J9XxR_culoezre_7R3eM0xKt02F-0TW2tgmIWG4hQT8";
-
 export const supabase = createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
+  "https://manobzroedsaxagugcvn.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1hbm9ienJvZWRzYXhhZ3VnY3ZuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgwNzA5MTMsImV4cCI6MjA4MzY0NjkxM30.J9XxR_culoezre_7R3eM0xKt02F-0TW2tgmIWG4hQT8"
 );

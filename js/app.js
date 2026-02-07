d9f0a9d4b3ed5fd5122cf2534437a0f23b124ecd
PLASENCIAHANDBALL <christianlinarescarsi@gmail.com>
2026-02-07 14:58:53 +0100

    Actualizar app.js

--- js/app.js
+++ js/app.js
@@ -179,6 +179,11 @@ function mostrarHome() {
       </div>
     </section>
 
+// 🔴 ACTUALIZACIONES EN VIVO (PRIORIDAD)
+html += `
+  <section class="actualizaciones-home" id="bloque-actualizaciones"></section>
+`;
+
     <!-- PATROCINADORES -->
     <h3>Patrocinadores</h3>
     <div class="patrocinadores-scroll">
@@ -204,11 +209,6 @@ function mostrarHome() {
   if (adminActivo) {
     html += `<button onclick="formNuevoPatrocinador()">➕ Añadir patrocinador</button>`;
   }
-
-// ACTUALIZACIONES EN VIVO
-html += `
-  <section class="actualizaciones-home" id="bloque-actualizaciones"></section>
-`;
 
   // HISTORIA AL FINAL
   html += `
